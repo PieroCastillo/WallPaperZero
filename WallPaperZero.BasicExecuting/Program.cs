8adedff15a2086f0b5259ada14c7b413b9f5ae61
Piero Castillo <pierocastillo2005@gmail.com>
2021-09-18 00:07:40 -0500

    works good, https://youtu.be/uvv7t2hYkF4

---
+++ WallPaperZero.BasicExecuting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WallPaperZero.Core;
+
+namespace WallPaperZero.BasicExecuting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //loads a dll with a WallPaper (should have an attribute)
+            WallPaperLoader.LoadAndRun(@"D:\My Projects\WallPaperZero\WallPapers\SolidColorsWallPaper\bin\Debug\net5.0\SolidColorsWallPaper.dll");
+        }
+    }
+}
---
+++ WallPapers/SolidColorsWallPaper/WallPaper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vortice.Direct2D1;
+using WallPaperZero.Core;
+
+namespace SolidColorsWallPaper
+{
+    [WallPaper]
+    public class SolidColorsWallPaper : WallPaper
+    {
+        public override void Load(ID2D1Factory factory, ID2D1HwndRenderTarget renderTarget)
+        {
+
+        }
+
+        public override void Render(ID2D1HwndRenderTarget renderTarget, long deltaTime)
+        {
+            renderTarget.BeginDraw();
+            var r = new RectangleF(new(0, 0), renderTarget.Size);
+            renderTarget.FillRectangle(in r, renderTarget.CreateSolidColorBrush(Color.Yellow));
+            Console.WriteLine("rendered frame");
+            renderTarget.EndDraw();
+        }
+    }
+}
