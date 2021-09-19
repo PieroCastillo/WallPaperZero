1248770a54f832c8a86645e327ba60aad02cbcdf
Piero Castillo <pierocastillo2005@gmail.com>
2021-09-18 21:15:28 -0500

    basic image wallpaper works ok

---
+++ WallPaperZero.Core/D2DUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vortice.Direct2D1;
+using Vortice.WIC;
+
+namespace WallPaperZero.Core
+{
+    public static class D2DUtils
+    {
+        public static ID2D1Bitmap LoadBitmapFromStream(this ID2D1HwndRenderTarget renderTarget, Stream data)
+        {
+            var wicFactory = new IWICImagingFactory();
+
+            var decoder = wicFactory.CreateDecoderFromStream(data);
+
+            var frame = decoder.GetFrame(0);
+
+            var formatConv = wicFactory.CreateFormatConverter();
+
+            formatConv.Initialize(frame, PixelFormat.Format32bppPBGRA, BitmapDitherType.None, null, 0f, BitmapPaletteType.Custom);
+
+            var rawImg = wicFactory.CreateBitmapFromSource(formatConv, BitmapCreateCacheOption.CacheOnDemand);
+
+            return renderTarget.CreateBitmapFromWicBitmap(rawImg, null);
+        }
+    }
+}
--- WallPapers/ImageWallPaper/WallPaper.cs
+++ WallPapers/ImageWallPaper/WallPaper.cs
@@ -7,10 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Vortice;
 using Vortice.Direct2D1;
-using Vortice.WIC;
 using WallPaperZero.Core;
-using BitmapInterpolationMode = Vortice.Direct2D1.BitmapInterpolationMode;
-using PixelFormat = Vortice.DCommon.PixelFormat;
 
 namespace ImageWallPaper
 {
@@ -18,39 +15,19 @@ namespace ImageWallPaper
     public class ImageWallPaper : WallPaper
     {
         ID2D1Bitmap image;
+
         public override void Load(ID2D1Factory factory, ID2D1HwndRenderTarget renderTarget)
         {
             var imgStream = Assembly.GetAssembly(this.GetType()).GetManifestResourceStream("ImageWallPaper.Assets.strelitzia_wallpaper.jpg");
-
-            var wicFactory = new IWICImagingFactory();
-
-            var decoder = wicFactory.CreateDecoderFromStream(imgStream);
-           
-            var frame = decoder.GetFrame(0);
-
-            var formatConv = wicFactory.CreateFormatConverter();
-
-            renderTarget.GetDpi(out float dpiX, out float dpiY);
-
-            Console.WriteLine($"dpiX : {dpiX}");
-            Console.WriteLine($"dpiY : {dpiY}");
-
-            formatConv.Initialize(frame, Vortice.WIC.PixelFormat.Format32bppPBGRA, BitmapDitherType.DualSpiral8x8, null, 0f, BitmapPaletteType.Custom);
-
-            var rawImg = wicFactory.CreateBitmapFromSource(formatConv, BitmapCreateCacheOption.CacheOnDemand);
-
-            PixelFormat pixelFormat = new PixelFormat();
-
-            image = renderTarget.CreateBitmapFromWicBitmap(rawImg, new(pixelFormat, 999,999));//renderTarget.Dpi.Width, renderTarget.Dpi.Height));
+            image = renderTarget.LoadBitmapFromStream(imgStream);
         }
-
 
         public override void Render(ID2D1HwndRenderTarget renderTarget, long deltaTime)
         {
             renderTarget.BeginDraw();
             var rect = new RawRectF(0, 0, renderTarget.Size.Width, renderTarget.Size.Height);
-            renderTarget.DrawBitmap(image, rect, 1f, BitmapInterpolationMode.Linear, rect);
-            renderTarget.DrawRectangle(in rect, renderTarget.CreateSolidColorBrush(Color.Blue));
+            var srcRect = new RectangleF(new(0, 0), image.PixelSize);
+            renderTarget.DrawBitmap(image, rect, 1f, BitmapInterpolationMode.NearestNeighbor, srcRect);
             renderTarget.EndDraw();
         }
     }
